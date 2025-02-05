dcd67b8a826885608b0dfb4033e09e57e2f5936a
maximegii <maxime.girardet@ecole-isitech.fr>
2025-02-05 12:20:44 +0100

    trying to fix build

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { signIn } from '../../../auth';
+
+export default function SignupForm(): React.ReactElement {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    await signIn(undefined, formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" placeholder="Name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" placeholder="Email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+      </div>
+    </form>
+  );
+}
